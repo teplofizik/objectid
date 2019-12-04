4b63cd1dab33999fc9b18ac77323edcb5180f04a
Alexey Sonkin <a.sonkin@incoel.ru>
2019-12-04 10:09:33 +0300

    added trainer params

--- train/trainer.py
+++ train/trainer.py
@@ -1,11 +1,12 @@
 
-from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from objectid.utils.dataset import ObjectDataset, DatasetSequence
 
 #example
 # trainer = Trainer(final_model)
 # trainer.earlyStopping()
 # trainer.modelCheckpoint('best_model.h5')
+# trainer.reduceLROnPlateau()
 # trainer.run(train, val)
 
 class Trainer:
@@ -14,12 +15,17 @@ class Trainer:
     self.model = model
     self.earlystopping = None
     self.modelcheckpoint = None
+    self.reducelronplateau = None
+    # LearningRateScheduler
 
   def earlyStopping(self):
     self.earlystopping = EarlyStopping(monitor='val_loss', patience=4)
 
-  def modelcheckpoint(self,filename):
+  def modelCheckpoint(self,filename):
     self.modelcheckpoint = ModelCheckpoint(filepath=filename, monitor='val_loss', save_best_only=True)
+
+  def reduceLROnPlateau(self):
+    self.reducelronplateau = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=8, verbose=0, mode='auto', min_delta=0.0001, cooldown=0, min_lr=0)
 
   def run(self, train, val):
     gen = DatasetLongSequence(train,15000,200)
@@ -29,5 +35,7 @@ class Trainer:
       callbacks.append(self.earlystopping)
     if self.modelcheckpoint is not None:
       callbacks.append(self.modelcheckpoint)
+    if self.reducelronplateau is not None:
+      callbacks.append(self.reducelronplateau)
 
     outputs = self.model.fit_generator(gen, steps_per_epoch=20, epochs=50, validation_data = val_gen, validation_steps=20,callbacks=callbacks)
